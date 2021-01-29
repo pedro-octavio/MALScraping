800cd3f2399b930a559e8f2472a0aab78b302aba
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-29 13:56:30 -0300

    Favorites Entities created

---
+++ MALScraping/Entities/Favorites/AnimeFavoritesEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace MALScraping.Entities.Favorites
+{
+    public class AnimeFavoritesEntity
+    {
+        public AnimeFavoritesEntity(string name, string midia, int year, string url, string image)
+        {
+            Name = name;
+            Midia = midia;
+            Year = year;
+            URL = url;
+            Image = image;
+        }
+
+        public string Name { get; private set; }
+        public string Midia { get; private set; }
+        public int Year { get; private set; }
+        public string URL { get; private set; }
+        public string Image { get; private set; }
+    }
+}
---
+++ MALScraping/Entities/Favorites/PeopleFavoiritesEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace MALScraping.Entities.Favorites
+{
+    public class PeopleFavoiritesEntity
+    {
+        public PeopleFavoiritesEntity(string name, string url, string image)
+        {
+            Name = name;
+            URL = url;
+            Image = image;
+        }
+
+        public string Name { get; private set; }
+        public string URL { get; private set; }
+        public string Image { get; private set; }
+    }
+}
