030e23af94bfbe6eede799dc4ab7bc24c57dea2e
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-29 20:37:46 -0300

    Update WebClientService.cs

--- MALScraping/Shared/WebClientService.cs
+++ MALScraping/Shared/WebClientService.cs
@@ -15,13 +15,7 @@ namespace MALScraping.Shared
 
             htmlDocument.LoadHtml(webPage);
 
-            var nodeCollection = htmlDocument.GetElementbyId("content").ChildNodes[1].ChildNodes[3].ChildNodes;
-
-            switch (nodeCollection.Count)
-            {
-                case 9: return nodeCollection[1].ChildNodes[3].ChildNodes[1].ChildNodes;
-                default: return nodeCollection[3].ChildNodes[3].ChildNodes[1].ChildNodes;
-            }
+            return htmlDocument.GetElementbyId("content").ChildNodes[1].ChildNodes[3].ChildNodes;
         }
     }
 }
