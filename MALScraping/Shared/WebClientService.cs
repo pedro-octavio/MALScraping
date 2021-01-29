77f4e676f471a40466fb319924c977a0992cae80
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-29 20:33:01 -0300

    WebClientService created

---
+++ MALScraping/Shared/WebClientService.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+
+namespace MALScraping.Shared
+{
+    internal static class WebClientService
+    {
+        internal static HtmlNodeCollection GetNodeCollection(string userName)
+        {
+            var webClient = new WebClient();
+
+            var webPage = webClient.DownloadString($"https://myanimelist.net/profile/{userName}");
+
+            var htmlDocument = new HtmlDocument();
+
+            htmlDocument.LoadHtml(webPage);
+
+            var nodeCollection = htmlDocument.GetElementbyId("content").ChildNodes[1].ChildNodes[3].ChildNodes;
+
+            switch (nodeCollection.Count)
+            {
+                case 9: return nodeCollection[1].ChildNodes[3].ChildNodes[1].ChildNodes;
+                default: return nodeCollection[3].ChildNodes[3].ChildNodes[1].ChildNodes;
+            }
+        }
+    }
+}
