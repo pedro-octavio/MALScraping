65de54f0dd73a4d55165adfa644000b94906b866
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-29 14:08:33 -0300

    Statistics Entities created

---
+++ MALScraping/Entities/Statistics/AnimeStatsEntity.cs
@@ -0,0 +1,38 @@
+﻿namespace MALScraping.Entities.Statistics
+{
+    public class AnimeStatsEntity
+    {
+        public AnimeStatsEntity(string userName, double days, double meanScore, int watching, int completed, int onHold, int dropped, int planToWatch, int totalEntries, int rewatched, int episodes)
+        {
+            URL = $"https://myanimelist.net/animelist/t{userName}?status=7";
+
+            Days = days;
+            MeanScore = meanScore;
+
+            Watching = watching;
+            Completed = completed;
+            OnHold = onHold;
+            Dropped = dropped;
+            PlanToWatch = planToWatch;
+
+            TotalEntries = totalEntries;
+            Rewatched = rewatched;
+            Episodes = episodes;
+        }
+
+        public string URL { get; private set; }
+
+        public double Days { get; private set; }
+        public double MeanScore { get; private set; }
+
+        public int Watching { get; private set; }
+        public int Completed { get; private set; }
+        public int OnHold { get; private set; }
+        public int Dropped { get; private set; }
+        public int PlanToWatch { get; private set; }
+
+        public int TotalEntries { get; private set; }
+        public int Rewatched { get; private set; }
+        public int Episodes { get; private set; }
+    }
+}
---
+++ MALScraping/Entities/Statistics/MangaStatsEntity.cs
@@ -0,0 +1,37 @@
+﻿namespace MALScraping.Entities.Statistics
+{
+    public class MangaStatsEntity
+    {
+        public MangaStatsEntity(string userName, double days, double meanScore, int reading, int completed, int onHold, int droppped, int planToRead, int totalEntries, int reread, int chapters, int volumes)
+        {
+            URL = $"https://myanimelist.net/mangalist/{userName}?status=7";
+            Days = days;
+            MeanScore = meanScore;
+            Reading = reading;
+            Completed = completed;
+            OnHold = onHold;
+            Droppped = droppped;
+            PlanToRead = planToRead;
+            TotalEntries = totalEntries;
+            Reread = reread;
+            Chapters = chapters;
+            Volumes = volumes;
+        }
+
+        public string URL { get; private set; }
+
+        public double Days { get; private set; }
+        public double MeanScore { get; private set; }
+
+        public int Reading { get; private set; }
+        public int Completed { get; private set; }
+        public int OnHold { get; private set; }
+        public int Droppped { get; private set; }
+        public int PlanToRead { get; private set; }
+
+        public int TotalEntries { get; private set; }
+        public int Reread { get; private set; }
+        public int Chapters { get; private set; }
+        public int Volumes { get; private set; }
+    }
+}
