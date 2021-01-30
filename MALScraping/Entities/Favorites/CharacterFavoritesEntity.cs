b9b991cc5cc9ff9f16e7eeea70808e7ac325a45f
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-30 16:56:16 -0300

    Update CharacterFavoritesEntity.cs

--- MALScraping/Entities/Favorites/CharacterFavoritesEntity.cs
+++ MALScraping/Entities/Favorites/CharacterFavoritesEntity.cs
@@ -2,16 +2,18 @@
 {
     public class CharacterFavoritesEntity
     {
-        public CharacterFavoritesEntity(string name, string uRL, string workURL, string image)
+        public CharacterFavoritesEntity(string name, string url, string workName, string workURL, string image)
         {
             Name = name;
-            URL = uRL;
+            URL = url;
+            WorkName = workName;
             WorkURL = workURL;
             Image = image;
         }
 
         public string Name { get; private set; }
         public string URL { get; private set; }
+        public string WorkName { get; private set; }
         public string WorkURL { get; private set; }
         public string Image { get; private set; }
     }
