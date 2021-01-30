e508885815a1d051acad6e9903b608ae7cc4f0e9
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-30 15:17:41 -0300

    Create AnimeStatsService.cs

---
+++ MALScraping/Services/AnimeStatsService.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using MALScraping.Entities.Statistics;
+using MALScraping.Shared;
+using System;
+
+namespace MALScraping.Services
+{
+    public static class AnimeStatsService
+    {
+        public static AnimeStatsEntity Load(string userName)
+        {
+            var nodeCollection = AdaptNodeCollection(WebClientService.GetNodeCollection(userName));
+
+            return new AnimeStatsEntity(
+                userName,
+                Convert.ToDouble(nodeCollection[3].ChildNodes[1].ChildNodes[1].InnerText.Replace(".", ",")),
+                Convert.ToDouble(nodeCollection[3].ChildNodes[3].ChildNodes[3].InnerText.Replace(".", ",")),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[1].ChildNodes[0].ChildNodes[1].ChildNodes[0].InnerText),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[1].ChildNodes[1].ChildNodes[1].ChildNodes[0].InnerText),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[1].ChildNodes[2].ChildNodes[1].ChildNodes[0].InnerText),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[1].ChildNodes[3].ChildNodes[1].ChildNodes[0].InnerText),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[1].ChildNodes[4].ChildNodes[1].ChildNodes[0].InnerText),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[2].ChildNodes[0].ChildNodes[1].InnerText.Replace(",", "")),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[2].ChildNodes[1].ChildNodes[1].InnerText.Replace(",", "")),
+                Convert.ToInt32(nodeCollection[7].ChildNodes[2].ChildNodes[2].ChildNodes[1].InnerText.Replace(",", "")));
+        }
+
+        private static HtmlNodeCollection AdaptNodeCollection(HtmlNodeCollection nodeCollection)
+        {
+            if (nodeCollection.Count == 9) return nodeCollection[1].ChildNodes[3].ChildNodes[1].ChildNodes;
+            else return nodeCollection[3].ChildNodes[3].ChildNodes[1].ChildNodes;
+        }
+    }
+}
