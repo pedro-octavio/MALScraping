dda37aae323e8997b084779a76df140a885c1a5e
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-01-30 17:44:30 -0300

    Create NodeCollectionHelper.cs

---
+++ MALScraping/Helpers/NodeCollectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+
+namespace MALScraping.Helpers
+{
+    internal static class NodeCollectionHelper
+    {
+        internal static HtmlNodeCollection AdaptFavorites(HtmlNodeCollection nodeCollection)
+        {
+            if (nodeCollection.Count == 9) return nodeCollection[5].ChildNodes[1].ChildNodes;
+            else return nodeCollection[7].ChildNodes[1].ChildNodes;
+        }
+
+        internal static HtmlNodeCollection AdaptAnimeStats(HtmlNodeCollection nodeCollection)
+        {
+            if (nodeCollection.Count == 9) return nodeCollection[1].ChildNodes[3].ChildNodes[1].ChildNodes;
+            else return nodeCollection[3].ChildNodes[3].ChildNodes[1].ChildNodes;
+        }
+
+        internal static HtmlNodeCollection AdaptMangaStats(HtmlNodeCollection nodeCollection)
+        {
+            if (nodeCollection.Count == 9) return nodeCollection[1].ChildNodes[5].ChildNodes[1].ChildNodes;
+            else return nodeCollection[3].ChildNodes[5].ChildNodes[1].ChildNodes;
+        }
+    }
+}
