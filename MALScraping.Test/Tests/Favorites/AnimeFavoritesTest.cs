b1af38e48d7723f5082ff428dfd33455a1e250fb
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-10 15:03:50 -0300

    Create AnimeFavoritesTest.cs

---
+++ MALScraping.Test/Tests/Favorites/AnimeFavoritesTest.cs
@@ -0,0 +1,40 @@
+﻿using MALScraping.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Favorites
+{
+    [TestClass]
+    public class AnimeFavoritesTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves")]
+        public void GetAnimeFavoritesOfValidUser(string userName)
+        {
+            try
+            {
+                AnimeFavoritesService.Load(userName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa")]
+        public void GetAnimeFavoritesOfInvalidUser(string userName)
+        {
+            try
+            {
+                AnimeFavoritesService.Load(userName);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
