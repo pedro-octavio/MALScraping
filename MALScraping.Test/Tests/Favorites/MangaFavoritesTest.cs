6b83d9d49bb28e8106e63d5b58f99e366d4ffb6d
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-10 21:06:29 -0300

    Create MangaFavoritesTest.cs

---
+++ MALScraping.Test/Tests/Favorites/MangaFavoritesTest.cs
@@ -0,0 +1,40 @@
+﻿using MALScraping.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Favorites
+{
+    [TestClass]
+    public class MangaFavoritesTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves")]
+        public void GetMangaFavoritesOfValidUser(string userName)
+        {
+            try
+            {
+                MangaFavoritesService.Load(userName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa")]
+        public void GetMangaFavoritesOfInvalidUser(string userName)
+        {
+            try
+            {
+                MangaFavoritesService.Load(userName);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
