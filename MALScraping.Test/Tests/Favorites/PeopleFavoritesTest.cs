47c00a2f30b8f211837fbc68e28af3090c7f61a5
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-11 18:39:42 -0300

    Create PeopleFavoritesTest.cs

---
+++ MALScraping.Test/Tests/Favorites/PeopleFavoritesTest.cs
@@ -0,0 +1,40 @@
+﻿using MALScraping.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Favorites
+{
+    [TestClass]
+    public class PeopleFavoritesTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves")]
+        public void GetPeopleFavoritesOfValidUser(string userName)
+        {
+            try
+            {
+                PeopleFavoritesService.Load(userName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa")]
+        public void GetPeopleFavoritesOfInvalidUser(string userName)
+        {
+            try
+            {
+                PeopleFavoritesService.Load(userName);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
