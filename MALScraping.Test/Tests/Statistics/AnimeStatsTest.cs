9cf63328b5f9960dce4724ea42129f1c34f489e7
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-13 22:10:26 -0300

    Create AnimeStatsTest.cs

---
+++ MALScraping.Test/Tests/Statistics/AnimeStatsTest.cs
@@ -0,0 +1,40 @@
+﻿using MALScraping.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Statistics
+{
+    [TestClass]
+    public class AnimeStatsTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves")]
+        public void GetAnimeStatsOfValidUser(string userName)
+        {
+            try
+            {
+                AnimeStatsService.Load(userName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa")]
+        public void GetAnimeStatsOfInvalidUser(string userName)
+        {
+            try
+            {
+                AnimeStatsService.Load(userName);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
