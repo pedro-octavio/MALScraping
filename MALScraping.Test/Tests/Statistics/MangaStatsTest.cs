3f237d9b3531be7f9a69789e98d8d01b94959bcd
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-14 03:32:19 -0300

    Create MangaStatsTest.cs

---
+++ MALScraping.Test/Tests/Statistics/MangaStatsTest.cs
@@ -0,0 +1,40 @@
+﻿using MALScraping.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Statistics
+{
+    [TestClass]
+    public class MangaStatsTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves")]
+        public void GetMangaStatsOfValidUser(string userName)
+        {
+            try
+            {
+                MangaStatsService.Load(userName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa")]
+        public void GetMangaStatsOfInvalidUser(string userName)
+        {
+            try
+            {
+                MangaStatsService.Load(userName);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
