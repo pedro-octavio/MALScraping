477457918d557d5ebcc30c3ecd9a1fbe8c50b6fd
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-18 21:05:18 -0300

    compare project created

---
+++ MALScraping.Compare/Services/AnimeStatsCompareService.fs
@@ -0,0 +1,29 @@
+﻿namespace MALScraping.Compare.Services
+
+open MALScraping.Services
+
+open MALScraping.Compare.Models
+
+type AnimeStatsCompareService () = class
+
+    static member Compare (userName1 : string, userName2 : string) : AnimeStatsCompareModel =
+
+        let user1 = AnimeStatsService.Load userName1
+        let user2 = AnimeStatsService.Load userName2
+
+        let days = user1.Days - user2.Days
+        let meanScore = user1.MeanScore - user2.MeanScore
+
+        let watching = user1.Watching - user2.Watching
+        let completed = user1.Completed - user2.Completed
+        let onHold = user1.OnHold - user2.OnHold
+        let dropped = user1.Dropped - user2.Dropped
+        let planToWatch = user1.PlanToWatch - user2.PlanToWatch
+
+        let totalEntries = user1.TotalEntries - user2.TotalEntries
+        let reWatched = user1.Rewatched - user2.Rewatched
+        let episodes = user1.Episodes - user2.Episodes
+
+        new AnimeStatsCompareModel (days, meanScore, watching, completed, onHold, dropped, planToWatch, totalEntries, reWatched, episodes)
+
+end
---
+++ MALScraping.Test/Tests/Compare/MangaStatsCompareTest.cs
@@ -0,0 +1,56 @@
+﻿using MALScraping.Compare.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MALScraping.Test.Tests.Compare
+{
+    [TestClass]
+    public class MangaStatsCompareTest
+    {
+        [TestMethod]
+        [DataRow("AlexandreEsteves", "leootaco")]
+        public void GetMangaFavoritesOfValidUser(string userName1, string userName2)
+        {
+            try
+            {
+                var x = MangaStatsCompareService.Compare(userName1, userName2);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        [DataRow("ska01pelxa", "leootaco")]
+        public void GetMangaFavoritesOfFirstUserInvalid(string userName1, string userName2)
+        {
+            try
+            {
+                MangaStatsCompareService.Compare(userName1, userName2);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+
+        [TestMethod]
+        [DataRow("AlexandreEsteves", "ska01pelxa")]
+        public void GetMangaFavoritesOfSecondUserInvalid(string userName1, string userName2)
+        {
+            try
+            {
+                MangaStatsCompareService.Compare(userName1, userName2);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "The remote server returned an error: (404) Not Found.");
+            }
+        }
+    }
+}
